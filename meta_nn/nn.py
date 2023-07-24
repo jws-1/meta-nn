fbe81a9dbc7d50b44c0deb9fceb2ba21aeb559f4
Jared Swift <j.w.swift@outlook.com>
2023-07-24 17:41:31 +0100

    First working example.

--- meta_nn/nn.py
+++ meta_nn/nn.py
@@ -5,27 +5,22 @@ class MetaNet(nn.Module):
     def __init__(self, state_dim, action_dim, hidden_dim=128):
         super(MetaNet, self).__init__()
 
-        # Define the input dimensions (state_dim + action_dim) for the state-action pair
-        input_dim = state_dim + action_dim
-
         # Define the hidden layer dimensions
         hidden_layer_1 = hidden_dim
         hidden_layer_2 = hidden_dim
 
         # Define the layers of the neural network
         self.layers = nn.Sequential(
-            nn.Linear(input_dim, hidden_layer_1),
+            nn.Linear(2, hidden_layer_1),
             nn.ReLU(),
             nn.Linear(hidden_layer_1, hidden_layer_2),
             nn.ReLU(),
-            nn.Linear(hidden_layer_2, state_dim)
+            nn.Linear(hidden_layer_2, 1)
         )
 
-    def forward(self, state, action):
-        # Concatenate state and action to create the input for the neural network
-        input_data = torch.cat([state, action], dim=-1)
+    def forward(self, X):
 
         # Pass the input through the neural network
-        next_state_prediction = self.layers(input_data)
+        next_state_prediction = self.layers(X)
 
-        return next_state_prediction
+        return next_state_prediction.view(-1)
